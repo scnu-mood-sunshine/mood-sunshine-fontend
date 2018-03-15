c7116cfc100e6b82c9a9a0c71de5c98701d4de54
Fon <1090737707@qq.com>
2018-03-15 17:10:41 +0800

    文章浏览页面更新

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,14 @@ require('styles/App.css');
 import React from 'react';
 import ArticleList from './main-page.js'
 import Login from './login-page.js'
-// import MoodWall from './mood-wall.js'
+import MoodWall from './mood-wall.js'
+import ArticleReading from './article-reading-page.js'
 import Test from './test'
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <Login />
+      <ArticleReading />
     );
   }
 }
