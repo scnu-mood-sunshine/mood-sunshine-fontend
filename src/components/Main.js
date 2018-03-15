11554833fc2bcddf87664bedc5b19e5fd9ef62d8
Fon <1090737707@qq.com>
2018-03-16 00:13:30 +0800

    导航栏布局更新

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,14 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import NavigationBar from './navigation-bar.js'
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <NavigationBar />
     );
   }
 }
