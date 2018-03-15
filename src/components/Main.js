ad8e7438f1cfce7aaa95a8c8ed267e7ded6ca69e
Fon <1090737707@qq.com>
2018-03-15 15:39:16 +0800

    登录页面更新

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,14 @@ require('styles/App.css');
 
 import React from 'react';
 import ArticleList from './main-page.js'
+import Login from './login-page.js'
 // import MoodWall from './mood-wall.js'
 import Test from './test'
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <ArticleList />
+      <Login />
     );
   }
 }
---
+++ src/components/login-page.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+require('../styles/login-page.css');
+//import registerServiceWorker from './registerServiceWorker';
+class Login extends Component{
+    render(){
+        return(
+            <div>
+                <div className='web-description'>
+                    <div className='title'>心晴</div>
+                    <div className='small-title'>Mood note</div>
+                    <div className='text'>愿您每一天都有好心情</div>
+                </div>
+                <div className="login-relogin-box">
+                    <div className='login-box'>
+                        <div className='normal-title'>
+                            <div className='login-relogin-title'>登录</div>
+                            <div className='login-relogin-title'>注册</div>
+                        </div>
+                        <div className='login-form-box'>
+                            <input type='text' value='手机号或邮箱'/>
+                            <input type='text' value='密码'/>
+                            <button className='login-button' type='button'>登录</button>
+                        </div>
+                        <div className='relogin-form-box'>
+                            <input type='text' value='昵称'/>
+                            <input type='text' value='手机号或邮箱'/>
+                            <input type='text' value='密码'/>
+                            <button className='login-button' type='button'>注册</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Login;
